0862267eee2064fed90163a5f199ee56aabc0aa3
Tony K. <tony-koke@outlook.com>
2023-12-12 14:00:47 +0000

    First Commit

---
+++ src/board/board.js
@@ -0,0 +1,7 @@
+export class Tile {
+    constructor(position, space, availability) {
+        this._position = position;
+        this._space = space;
+        this._availability = availability;
+    }
+}

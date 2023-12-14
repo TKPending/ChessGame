d45d6bc75118865994b654791d02815ff9322316
Tony K. <tony-koke@outlook.com>
2023-12-14 19:23:14 +0000

    Basic highlight feature

--- src/util/clickedPiece.js
+++ src/util/clickedPiece.js
@@ -21,19 +21,46 @@ const pressedElement = (clickedElement) => {
     }
 }
 
+// Helper function to highlight a tile
+const highlightTile = (tilePosition) => {
+    const tileElement = document.getElementById(tilePosition);
+    if (tileElement) {
+        tileElement.classList.add('highlighted');
+    }
+};
+
+
+// Helper function to remove highlight class from all tiles
+const removeAllHighlightClasses = (chessBoard) => {
+    chessBoard.forEach(row => {
+        row.forEach(tile => {
+            const tileElement = document.getElementById(tile.position);
+            if (tileElement) {
+                tileElement.classList.remove('highlighted');
+            }
+        });
+    });
+};
+
 // Return piece or tile
 export const pressedTile = (event, chessBoard) => {
     const clickedElement = event.target;
     const tilePosition = pressedElement(clickedElement);
-    const clickedTile = findTileByPosition(chessBoard, tilePosition)
+    const clickedTile = findTileByPosition(chessBoard, tilePosition);
+
+    // Remove highlight class from all tiles
+    removeAllHighlightClasses(chessBoard);
 
     // Check if the tile exists and has a piece
     if (clickedTile && clickedTile.spaceOccupation) {
         console.log(`Piece in clicked tile (${tilePosition}) :`, clickedTile.spaceOccupation);
+        highlightTile(tilePosition); // Pass tile position, not tile element
         return clickedTile.spaceOccupation;
     } else {
         console.log(`No piece in clicked tile or invalid tile. (${tilePosition})`);
         console.log(clickedTile)
+        highlightTile(tilePosition); // Pass tile position, not tile element
         return clickedTile;
     }
 }
+

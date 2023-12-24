dd8084a733801b9948562701871fb63c280c9162
Tony K. <tony-koke@outlook.com>
2023-12-24 01:14:55 +0000

    Added extra knight moves

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -1,3 +1,4 @@
+// knight.js
 import { Piece } from "./pieces.js";
 import { highlightTile } from "../util/clickedPiece.js";
 import { indexToLocation } from "../util/findLocation.js";
@@ -17,6 +18,7 @@ export class Knight extends Piece {
 
         return this.pieceBoundCheck(newRow, newCol);
     }
+
     jumpUpLeft() {
         const [row, col] = this._currentPosition;
         this._lastPosition = [row, col];
@@ -47,6 +49,47 @@ export class Knight extends Piece {
         return this.pieceBoundCheck(newRow, newCol);
     }
 
+    // Additional L-shaped Moves
+    jumpRightUp() {
+        const [row, col] = this._currentPosition;
+        this._lastPosition = [row, col];
+
+        const newRow = row - 1 * this.direction;
+        const newCol = col + 2 * this.direction;
+
+        return this.pieceBoundCheck(newRow, newCol);
+    }
+
+    jumpRightDown() {
+        const [row, col] = this._currentPosition;
+        this._lastPosition = [row, col];
+
+        const newRow = row + 1 * this.direction;
+        const newCol = col + 2 * this.direction;
+
+        return this.pieceBoundCheck(newRow, newCol);
+    }
+
+    jumpLeftUp() {
+        const [row, col] = this._currentPosition;
+        this._lastPosition = [row, col];
+
+        const newRow = row - 1 * this.direction;
+        const newCol = col - 2 * this.direction;
+
+        return this.pieceBoundCheck(newRow, newCol);
+    }
+
+    jumpLeftDown() {
+        const [row, col] = this._currentPosition;
+        this._lastPosition = [row, col];
+
+        const newRow = row + 1 * this.direction;
+        const newCol = col - 2 * this.direction;
+
+        return this.pieceBoundCheck(newRow, newCol);
+    }
+
     // Generate all legal moves for the knight
     generateLegalMoves() {
         const legalMoves = [
@@ -54,6 +97,10 @@ export class Knight extends Piece {
             this.jumpUpLeft(),
             this.jumpDownRight(),
             this.jumpDownLeft(),
+            this.jumpRightUp(),
+            this.jumpRightDown(),
+            this.jumpLeftUp(),
+            this.jumpLeftDown(),
         ];
 
         // Filter out null moves (moves outside the chessboard)

d63e1a47696380c76c383e3a25b7147bc2aa40d8
Tony K. <tony-koke@outlook.com>
2023-12-28 13:17:41 +0000

    Tiles will now initialise with piece name

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ initializeBoardWithPieces(chessBoard)
 
 renderChessboard(chessBoard);
 
+
+console.log(chessBoard);
+
 const bodyElement = document.getElementById('body');
 const chessBoardElement = document.querySelector('.chess-board');
 
